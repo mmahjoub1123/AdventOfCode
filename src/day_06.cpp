fedf7eb69dc2d0e1a40beb6de92a5c611ac44c2e
mmahjoub1123 <mahjoub.myriam@gmail.com>
2022-12-15 15:09:11 +0100

    add day_06

---
+++ src/day_06.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+// Parse the input file
+string parseFile(string inFileName)
+{
+    ifstream inFile;
+
+    inFile.open(inFileName);
+
+    string buffer;
+
+    if (inFile.is_open())
+    {
+        while (!inFile.eof())
+        {
+            getline(inFile, buffer);
+        }
+
+        inFile.close();
+    }
+    else
+    {
+        cout << "Cannot open file: " << inFileName << endl;
+    }
+    return buffer;
+}
+
+
+int get_marker(string buffer, int nb_distinct)
+{
+    int index = 0;
+    bool is_marker = true;
+    while (index <= buffer.length())
+    {
+        string temp_str = buffer.substr(index, nb_distinct);
+        sort(temp_str.begin(), temp_str.end());
+
+        for (int i=0; i < temp_str.length()-1; i++)
+        {
+            if (temp_str[i] == temp_str[i+1])
+            {
+                is_marker = false;
+                break;
+            }
+        }
+        if (is_marker)
+        {
+            break;
+        }
+        is_marker = true;
+        index++;
+    }
+    return index+nb_distinct;
+}
+
+
+int part1(string buffer)
+{
+    return get_marker(parseFile(buffer), 4);
+}
+
+
+int part2(string buffer)
+{
+    return get_marker(parseFile(buffer), 14);
+}
+
+
+int main(int argc, char **argv)
+{
+    cout << "Part 1: " << part1(argv[1]) << endl;
+    cout << "Part 2: " << part2(argv[1]) << endl;
+
+    return 0;
+}
